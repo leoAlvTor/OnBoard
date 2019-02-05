c511e82a9684b769bbcbf70897f32d72d46144ae
leoAlvTor <calvaradot1@est.ups.edu.ec>
2019-02-04 20:39:06 -0500

    Beginning the update section.

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -1,10 +1,13 @@
 package vista;
 
+import controlador.MongoDBConection;
 import javafx.application.Application;
 import vista.logIn.StartUpTest;
 
 public class Main {
     public static void main(String[] args) {
-        new Thread(() -> Application.launch(StartUpTest.class)).start();
+        //new Thread(() -> Application.launch(StartUpTest.class)).start();
+
+        MongoDBConection mongoDBConection = new MongoDBConection();
     }
 }
